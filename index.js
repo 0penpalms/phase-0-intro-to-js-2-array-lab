c8b805bd94b610b1f2e5963f4f6eea4f3a902617
0penpalms <jamescartermvnyc@gmail.com>
2024-07-26 09:47:29 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function prependCat(name) {
+    return [name,...cats];
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+function removeLastCat() {
+    // return cats.slice(0, cats.length - 1)
+    return cats.slice(0, -1) 
+}
